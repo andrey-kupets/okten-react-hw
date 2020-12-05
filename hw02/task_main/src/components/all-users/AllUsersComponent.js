5de1465087b090b226508cbaf7e65d1111627392
andrey-kupets <andrey.kupets@i.ua>
2020-12-05 23:51:46 +0300

    add hw02 users highlight

--- hw02/task_main/src/components/all-users/AllUsersComponent.js
+++ hw02/task_main/src/components/all-users/AllUsersComponent.js
@@ -4,51 +4,52 @@ import './AllUsers.css';
 
 class AllUsersComponent extends Component {
 
-    state = {users: []};
+    state = {users: [], classState: 'one'};
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
             .then(usersFromAPI => {
-                this.setState({users: usersFromAPI, flag: false, classState: 'one', chosenUser: null});
-            });
-    };
+                this.setState({users: usersFromAPI})
+            })
+    }
 
     changeColor = () => {
         if (this.flag) {
-            this.setState({classState: 'one'});
-        }else {
-            this.setState({classState: 'two'});
+            this.setState( {classState: 'one'});
+        }else{
+            this.setState( {classState: 'two'});
         }
         this.flag = !this.flag;
     }
 
     selectThisUser = (id) => {
-        let chosenUser = this.state.users.find(value => value.id === id);
-        this.setState({chosenUser});
+        let chosenUser = this.state.users.find(value => value.id === id)
+        this.setState({chosenUser})
     }
 
     render() {
-        let {users, classState, chosenUser} = this.state;
+        let {classState, users, chosenUser} = this.state;
         return (
             <div>
-                <h1 onClick={this.changeColor} className={classState}>All Users Info</h1>
+                <h1 onClick={this.changeColor} className={classState}>All Users info</h1>
                 {
-                    users.map(value => (<UserComponent
+                    users.map(value => <UserComponent
                         item={value}
                         key={value.id}
                         selectThisUser={this.selectThisUser}
-                    />))
-                }
+                    />)}
 
                 <hr/>
 
                 {
-                    chosenUser && <UserComponent item={chosenUser}/>
+                     chosenUser && <UserComponent item={chosenUser}/>
                 }
             </div>
+
         );
     }
 }
 
 export default AllUsersComponent;
+
--- hw02/task_main/src/components/user/UserComponent.js
+++ hw02/task_main/src/components/user/UserComponent.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-
+import '../all-users/AllUsers.css';
 class UserComponent extends Component {
+    state = {classState: 'one'};
+    changeColor = () => {
+        if (this.flag) {
+            this.setState( {classState: 'one'});
+        }else{
+            this.setState( {classState: 'two'});
+        }
+        this.flag = !this.flag;
+    }
 
     render() {
         let {item, selectThisUser} = this.props;
-
+        let {classState} = this.state;
         return (
-            <div>
-                {item.id} - {item.name} - City: {item.address.city};
-                {selectThisUser && <button onClick={() => selectThisUser(item.id)}>Choose User</button>}
+
+            <div >
+
+                <h3 onClick={this.changeColor} className={classState}>
+                    {item.id} - {item.name} - City: {item.address.city};
+                    {selectThisUser && <button onClick={() => selectThisUser(item.id)}>Choose User</button>}
+
+                </h3>
             </div>
         );
     }
