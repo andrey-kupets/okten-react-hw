b7f94a7381e647da903af01051d14425104a551e
andrey-kupets <andrey.kupets@i.ua>
2020-12-06 23:23:28 +0300

    add hw03 repeat plus (all components)

--- hw03/task_main/src/App.js
+++ hw03/task_main/src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import AllUsers from "./components/all-users/AllUsers";
+import AllPosts from "./components/all-posts/AllPosts";
+import AllComments from "./components/all-comments/AllComments";
 
 class App extends Component {
     render() {
         return (
             <div>
-              <AllUsers/>
+                <AllUsers/>
+                <hr/>
+                <AllPosts/>
+                <hr/>
+                <AllComments/>
             </div>
         );
     }
