ed7b978b09493b5b9131daeeb3fca0dfa1fbbf09
andrey-kupets <andrey.kupets@i.ua>
2020-12-15 03:30:30 +0300

    add start form

--- hw06/task_main-form/src/App.js
+++ hw06/task_main-form/src/App.js
@@ -2,10 +2,11 @@ import React, {Component} from 'react';
 import AllUsers from "./components/AllUsers";
 import customContext from "./services/ContextService";
 import {Context} from "./services/ContextService";
+import User from "./components/User";
 
 class App extends Component {
-
-    state = {users: []};
+    myForm = React.createRef();
+    state = {users: [], inputValue: ''};
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -21,10 +22,34 @@ class App extends Component {
             <Context.Provider value={<b>'Pass Through'</b>}>
                 <div>
                     <AllUsers items={users}/>
+                    <form action={'/savedata'} onSubmit={this.send} ref={this.myForm}>
+                        <input type = "number" onInput={this.commitState} value={this.state.inputValue}/>
+                        <button onSubmit={this.getUser}>send</button>
+                    </form>
+                    <User item={users}/>
                 </div>
             </Context.Provider>
         );
     }
+
+    send = (e) => {
+        e.preventDefault();
+        // console.log(this.myForm.current[0].value);
+        console.log(e.target.children[0].value);
+    };
+
+    commitState = (e) => {
+        this.setState({inputValue: e.target.value});
+    }
+
+    getUser = () => {
+        const {inputValue, users} = this.state;
+        if (inputValue) {
+            users.map(value => value.id === inputValue)
+            return <User/>
+        }
+    }
+
 }
 
 export default App;
